20501b0cad5f69f4226d10b5dc33f868445e3369
Pan Cheng <pchengeducation@hotmail.com>
2018-08-17 17:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function showRepositories(event, data){
   console.log(this.responsText)
-}
+  let repolist = "<ul>"
+  for (var i=0; i < this.responseText.length; i++){
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("respositories").innerHTML = repoList
+  }
 
 function getRepositories(){
   const req = new XMLHttpRequest()
