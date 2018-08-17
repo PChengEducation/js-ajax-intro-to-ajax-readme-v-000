f3a61256939e8894e59cd2724d7e99e7c778589d
Pan Cheng <pchengeducation@hotmail.com>
2018-08-17 17:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function getRepositories() {
+function showRepositories(event, data){
+  console.log(this.responsText)
+}
+
+function getRepositories(){
   const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octorcat/repos')
   req.send()
 }
